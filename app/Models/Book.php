8a8f15c14bf988b8458fec96f63ec186a971d9db
Zgrovetin <534477@gmail.com>
2024-09-26 18:02:45 +0200

    Two query builder functions added to Book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -10,7 +10,8 @@ class Book extends Model
 {
     use HasFactory;
 
-    public function reviews() {
+    public function reviews()
+    {
         return $this->hasMany(Review::class);
     }
 
@@ -18,4 +19,15 @@ class Book extends Model
     {
         return $query->where('title', 'LIKE', '%' . $title . '%');
     }
+
+    public function scopePopular(Builder $query): Builder
+    {
+        return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder
+    {
+        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
+    }
+
 }
