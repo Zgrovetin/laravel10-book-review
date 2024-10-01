ec828bc95d63e4148c2f36d57dc701c932490492
Zgrovetin <534477@gmail.com>
2024-10-01 12:25:26 +0200

    3 scenarios of data editing

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -27,7 +27,7 @@ class BookController extends Controller
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
             default => $books->latest()
         };
-        
+
         $cacheKey = 'books:' . $filter . ':' . $title;
         $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
 
@@ -55,14 +55,11 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        return view(
-            'books.show',
-            [
-                'book' => $book->load([
-                    'reviews' => fn($query) =>$query->latest()
-                ])
-            ]
-        );
+       $cacheKey = 'book:' . $book->id;
+
+       $book = cache()->remember($cacheKey, 3600, fn() => $book->load(['reviews' => fn($query) => $query->latest()]));
+
+        return view('books.show', ['book' => $book]);
     }
 
     /**
